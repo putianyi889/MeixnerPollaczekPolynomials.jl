647d49443a678c0b559543382113a2e710a174fe
Tianyi Pu <912396513@qq.com>
2024-11-08 09:28:32 +0000

    fix

--- src/MeixnerPollaczekPolynomials.jl
+++ src/MeixnerPollaczekPolynomials.jl
@@ -1,9 +1,9 @@
 module MeixnerPollaczekPolynomials
 
-import Base: axes, getindex, oneto
+import Base: axes, getindex, oneto, ==
 import ClassicalOrthogonalPolynomials: grammatrix, Inclusion, jacobimatrix, orthogonalityweight, OrthogonalPolynomial, Weight, ℝ, ∞
 import LazyBandedMatrices: Tridiagonal
-import RationalVals: RationalVal
+import RationalVals: RationalVal, IntegerVal
 import SpecialFunctions: gamma
 
 export MeixnerPollaczek
@@ -17,19 +17,21 @@ MeixnerPollaczekWeight(λ::L, ϕ::P) where {L,P} = MeixnerPollaczekWeight{promot
 axes(::MeixnerPollaczekWeight{T}) where {T} = (Inclusion{T}(ℝ),)
 function getindex(w::MeixnerPollaczekWeight, x::Number)
     x ∈ axes(w, 1) || throw(BoundsError())
-    abs2(gamma(w.λ + x * im)) * exp(x * (2 * w.ϕ - π))
+    abs2(gamma(w.λ + x * im)) * exp(x * (IntegerVal{2}() * w.ϕ - π))
 end
 
 struct MeixnerPollaczek{T,L,P} <: OrthogonalPolynomial{T}
     λ::L
     ϕ::P
 end
-MeixnerPollaczek{T}(λ::L, ϕ::P) where {T,L,P}=MeixnerPollaczek{T,L,P}(λ,ϕ)
+MeixnerPollaczek{T}(λ::L, ϕ::P) where {T,L,P} = MeixnerPollaczek{T,L,P}(λ, ϕ)
 MeixnerPollaczek(λ::L, ϕ::P) where {L,P} = MeixnerPollaczek{promote_type(L, P)}(λ, ϕ)
+
+==(A::MeixnerPollaczek, B::MeixnerPollaczek) = A.λ == B.λ && A.ϕ == B.ϕ
 
 axes(::MeixnerPollaczek{T}) where {T} = (Inclusion{T}(ℝ), oneto(∞))
 orthogonalityweight(S::MeixnerPollaczek{T}) where {T} = MeixnerPollaczekWeight{T}(S.λ, S.ϕ)
 
-jacobimatrix(S::MeixnerPollaczek) = Tridiagonal(RationalVal{1}{2}()*csc(S.ϕ)*oneto(∞),-cot(S.ϕ)*(S.λ:∞), csc(S.ϕ)*(S.λ:∞))
+jacobimatrix(S::MeixnerPollaczek) = Tridiagonal(RationalVal{1,2}() * csc(S.ϕ) * oneto(∞), -cot(S.ϕ) * (S.λ:∞), RationalVal{1,2}() * csc(S.ϕ) * (IntegerVal{2}()*S.λ:∞))
 
 end
